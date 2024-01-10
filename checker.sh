688c054e7544240c9977034aac148895be7e762a
prukngan <phongsathon.rak2003@gmail.com>
2024-01-10 10:38:48 +0700

    #2

--- checker.sh
+++ checker.sh
@@ -5,39 +5,39 @@ GREEN='\e[0;32m'
 NC='\e[0m'
 
 echo "Hostname  : $(hostname)"
-echo "IP                : $(hostname -I)"
+echo "IP        : $(hostname -I)"
 
 echo -n "Static IP : "
-if ip r | grep -q 'proto static'; then
-        echo -e "${GREEN}Passed${NC}"
+if ip r | grep -q 'proto static' 2>/dev/null; then
+        echo "${GREEN}Passed${NC}"
 else
-        echo -e "${RED}Failed${NC}"
+        echo "${RED}Failed${NC}"
 fi
 
-echo -n "PermitRootLogin : "
-if grep -q "PermitRootLogin yes" /etc/ssh/sshd_config 2>/dev/null; then
-        echo -e "${GREEN}Yes${NC}"
+echo -n "PermitRootLogin deny : "
+if service ssh restart | grep -q "PermitRootLogin no" /etc/ssh/sshd_config 2>/dev/null; then
+        echo "${GREEN}Passed${NC}"
 else
-        echo -e "${RED}No${NC}"
+        echo "${RED}Failed${NC}"
 fi
 
 echo -n "Trusted 158.108.32.60 : "
-if head -q "158.108.32.60" /etc/bind/named.conf.options 2>/dev/null; then
-        echo -e "${GREEN}Passed${NC}"
+if grep -q "158.108.32.60" /etc/bind/named.conf.options 2>/dev/null; then
+        echo "${GREEN}Passed${NC}"
 else
-        echo -e "${RED}Failed${NC}"
+        echo "${RED}Failed${NC}"
 fi
 
 echo -n "Check http : "
-if lynx http://testssl.cpe36.net 2>/dev/null ; then
-        echo -e "${GREEN}Passed${NC}"
+if lynx -source http://testssl.cpe36.net >/dev/null 2>&1 ; then
+        echo "${GREEN}Passed${NC}"
 else
-        echo -e "${RED}Failed${NC}"
+        echo "${RED}Failed${NC}"
 fi
 
 echo -n "Check https : "
-if lynx https://testssl.cpe36.net 2>/dev/null ; then
-        echo -e "${GREEN}Passed${NC}"
+if lynx -source https://testssl.cpe36.net >/dev/null 2>&1 ; then
+        echo "${GREEN}Passed${NC}"
 else
-        echo -e "${RED}Failed${NC}"
+        echo "${RED}Failed${NC}"
 fi
